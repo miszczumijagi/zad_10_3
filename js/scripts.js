a8c9baa148e0da7792d681c6bfe89c374045cb69
Kamil Stangret <kamilstangret@wp.pl>
2018-01-09 19:52:54 +0100

    Add content responsible for pagination

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,77 @@ $(function(){
 		intervalId = setInterval(changeSlide, 3000); 
 		
 	};
+
 	
 	function changeSlide() {
-			carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+
+		changePagination();
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 			
+	
+		function moveFirstSlide() {
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
+
+			lastItem.after(firstItem);
+			carouselList.css({marginLeft:0});
+			changePagination();
+		};
+	};
+	
+	function changeSlideBack(){
+
+		changePagination();
+	    carouselList.animate({'marginLeft':0}, 500, moveFirstSlideBack);
+	
+
+		function moveFirstSlideBack(){
+		    var firstItem = carouselList.find("li:first");
+		    var lastItem = carouselList.find("li:last");
+		    firstItem.before(lastItem);
+		    carouselList.css({marginLeft:-400});
+		};
+	    
 	};
 
-	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
+	$('#goRight').click(function(){
+			
+	    changeSlide();
+	    stopInterval();
+	    
+	  });
 
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});	
-	}
+	$('#goLeft').click(function(){
+			
+	    changeSlideBack();
+	    stopInterval();
+	    
+	});
+
 	setDefaultInterval();
+
+	function stopInterval() {
+		clearInterval(intervalId);
+	};
+
+	
+	function changePagination() {
+		var currentSlide = carouselList.find("li:first");
+		var clickedButton = $('.change-slide');
+		var currentSlideNum = currentSlide.attr('data-slide');
+
+		clickedButton.each(function(index, element) {
+			if ((index + 1) == currentSlideNum) {
+				$(element).addClass('active')
+			} else {
+				$(element).removeClass('active');
+			}
+
+		})
+	}
+	
 	
 });
-
-
 
 
 
